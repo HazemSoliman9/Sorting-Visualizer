e741d077c1f2b03edb0bbe9bad8b9dd6a11f5f1e
HazemSoliman9 <73444358+HazemSoliman9@users.noreply.github.com>
2020-11-13 22:04:35 +0200

    Add files via upload
    To Do:
    Add event listener to the "generate" button
    Make array size dynamic according to slider

---
+++ script.js
@@ -0,0 +1,25 @@
+window.onload = function () {
+  const control = document.getElementById("control");
+  const algorithm_container = document.querySelector("algorithm_container");
+  const array_size = document.querySelector("array_size");
+  const sort_speed = document.querySelector("sort_speed");
+  const generate_rand_array_btn = document.getElementById("generate");
+  const array_container = document.getElementById("array_container");
+
+  generate_rand_array_btn.addEventListener("onclick", function (e) {
+    console.log("eh yalaaaa");
+    // generateArray();
+  });
+  function generateArray() {
+    array_container.innerHTML = "";
+    for (let i = 0; i < 82; i++) {
+      var height = Math.floor(Math.random() * Math.floor(99)) + 1;
+      var bar = document.createElement("div");
+      bar.style.height = height + "%";
+      bar.style.top = 100 - height + "%";
+      bar.id = "bar";
+      document.getElementById("array_container").appendChild(bar);
+    }
+  }
+  generateArray();
+};
