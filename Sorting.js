fa541d6d0fea4a4ea1541ae28de8c28bb8b8f8eb
Khaled Nowara <kn_nowara@outlook.com>
2020-11-20 22:15:57 +0200

    another recursive test function

--- Sorting.js
+++ Sorting.js
@@ -30,7 +30,18 @@ function bubbleSortRec (myArray, n ){ // pass length of array as n
 
 }
 
+function recursiveTest(myArray, n){ // recursive function to switch every 2 neighbouring elements for recursive animation test  
+    if (n <=1) return ;
+    else {  
+        temp = myArray[n-1];
+        myArray[n-1] = myArray [n-2];
+        myArray [n-2] = temp;
+        recursiveTest (myArray,n-2)
+
+    }
+}
+
 var array = [2,5,4,3,6,9,5,8,98,43,787];
 console.log(JSON.stringify(array));
-bubbleSortRec(array,11);
+recursiveTest(array,11);
 console.log(JSON.stringify(array));
